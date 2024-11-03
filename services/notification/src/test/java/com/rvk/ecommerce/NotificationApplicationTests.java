51cc087ccd792e26ccdc9ee05dddec675c4b43ea
RuchiraKarunatissa <ruchira.karunatissa@gmail.com>
2024-11-04 05:12:51 +0530

    test

---
+++ services/notification/src/test/java/com/rvk/ecommerce/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rvk.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
